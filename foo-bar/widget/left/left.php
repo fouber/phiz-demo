088b59874b2205d58c219a1971b62d26502f04be
zhangyunlong <zhangyunlong@baidu.com>
2013-10-21 02:20:47 +0800

    update readme

--- foo-bar/widget/left/left.php
+++ foo-bar/widget/left/left.php
@@ -8,3 +8,9 @@
 <p>Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
 <p class="text-info">Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla.</p>
 <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.</p>
+
+<?php $this->startScript()?>
+<script type="text/javascript">
+    console.log('left loaded');
+</script>
+<?php $this->endScript()?>
